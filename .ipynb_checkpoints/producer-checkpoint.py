6b2a7d587117a4c1a86d277b37df4be0de952d69
Sai-Srinivasa-S-P <sapu6645@colorado.edu>
2025-02-04 22:30:15 +0000

    v1

---
+++ .ipynb_checkpoints/producer-checkpoint.py
@@ -0,0 +1,30 @@
+from confluent_kafka import Producer 
+import json
+import random 
+import time 
+
+
+conf = {'bootstrap.servers': 'YOUR_EVENT_HUB_ENDPOINT', 'security.protocol': 'SASL_SSL',
+        'sasl.mechanisms': 'PLAIN', 'sasl.username': '$ConnectionString',
+        'sasl.password': 'YOUR_EVENT_HUB_CONNECTION_STRING'}
+
+producer = Producer(conf)
+
+## function to generate random data for real time 
+
+def generate_sales_data():
+    return {
+    "product_id": random.radint(1,1000),
+    "store_id": random.randint(1,100),
+    "timestamp" : time.time(),
+    "quantity_sold" : random.randint(1,10)
+    }
+
+
+## data is streamed  
+
+while True:
+    data = generate_sales_data()
+    producer.produce("sales_data",key=str(data["product_id"]),value=json.dumps(data))
+    producer.flush()
+    time.sleep(1)
